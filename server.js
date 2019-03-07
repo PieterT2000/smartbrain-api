458023898e0dc670b9d52b0c4cfeda7026d0215f
PieterT2000 <pietervantuijl2000@gmail.com>
2019-03-07 11:28:29 +0100

    finish endpoints

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
 	users: [
@@ -22,6 +25,13 @@ const database = {
 			entries: 0,
 			joined: new Date()
 		}
+	],
+	login: [
+		{
+			id: '987',
+			hash: '',
+			email: 'john@gmail.com'
+		}
 	]
 };
 
@@ -30,8 +40,14 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
+	bcrypt.compare('apples', '$2a$10$XUjuiCkyAXPVXQsXIOdNpewEAUgyKQpr8L8MxuOfqj/0hszc9uxo6', function(err, res) {
+		console.log('first guess', res);
+	});
+	bcrypt.compare('bacon', '$2a$10$XUjuiCkyAXPVXQsXIOdNpewEAUgyKQpr8L8MxuOfqj/0hszc9uxo6', function(err, res) {
+		console.log('first guess', res);
+	});
 	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-		res.json('success');
+		res.json(database.users[0]);
 	} else {
 		res.status(400).json('error logging in');
 	}
@@ -39,6 +55,10 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
+	bcrypt.hash(password, null, null, function(err, hash) {
+		// Store hash in your password DB.
+		console.log(hash);
+	});
 	database.users.push({
 		id: '125',
 		name,
@@ -50,16 +70,35 @@ app.post('/register', (req, res) => {
 	res.json(database.users[database.users.length - 1]);
 });
 
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach((user) => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		}
+	});
+	if (!found) {
+		res.status(404).json('no such user');
+	}
+});
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach((user) => {
+		if (user.id === id) {
+			found = true;
+			user.entries++;
+			return res.json(user.entries);
+		}
+	});
+	if (!found) {
+		res.status(404).json('no found');
+	}
+});
+
 app.listen(3001, () => {
 	console.log("Cheers, you're server is running on port 3001");
 });
-
-/*
-
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-
-*/
