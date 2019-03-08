b96d4d7f27876c1ee9716a7aa3df020a485b3d57
PieterT2000 <pietervantuijl2000@gmail.com>
2019-03-08 13:50:29 +0100

    code refactor + security review

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+//require routes
+const register = require('./controllers/register'),
+	signin = require('./controllers/signin'),
+	profile = require('./controllers/profile'),
+	image = require('./controllers/image');
+
 //set up postgreSQL with knex
 const db = knex({
 	client: 'pg',
@@ -19,90 +25,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.get('/', (req, res) => {
-// 	res.send(database.users);
-// });
-
-app.post('/signin', (req, res) => {
-	const { email, password } = req.body;
-	db
-		.select('email', 'hash')
-		.from('login')
-		.where('email', '=', email)
-		.then((data) => {
-			const isValid = bcrypt.compareSync(password, data[0].hash);
-			if (isValid) {
-				return db
-					.select('*')
-					.from('users')
-					.where('email', '=', email)
-					.then((user) => {
-						res.json(user[0]);
-					})
-					.catch((err) => res.status(400).json('unable to get user'));
-			} else {
-				res.status(400).json('wrong credentials');
-			}
-		})
-		.catch((err) => res.status(400).json('wrong credentials'));
-});
+app.post('/signin', signin.handleSignin(db, bcrypt));
 
 app.post('/register', (req, res) => {
-	const { email, name, password } = req.body;
-	const hash = bcrypt.hashSync(password);
-	db
-		.transaction((trx) => {
-			trx
-				.insert({
-					hash: hash,
-					email: email
-				})
-				.into('login')
-				.returning('email')
-				.then((loginEmail) => {
-					return trx('users')
-						.returning('*')
-						.insert({
-							email: loginEmail[0],
-							name: name,
-							joined: new Date()
-						})
-						.then((user) => {
-							res.json(user[0]);
-						});
-				})
-				.then(trx.commit)
-				.catch(trx.rollback);
-		})
-		.catch((err) => res.status(400).json('unable to register'));
+	register.handleRegister(req, res, db, bcrypt);
 });
 
 app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	db
-		.select('*')
-		.from('users')
-		.where({ id })
-		.then((user) => {
-			if (user.length) {
-				res.json(user[0]);
-			} else {
-				res.status(400).json('Not found');
-			}
-		})
-		.catch((err) => res.status(400).json('error getting user'));
+	profile.handleProfileGet(req, res, db);
 });
 
 app.put('/image', (req, res) => {
-	const { id } = req.body;
-	db('users')
-		.where('id', '=', id)
-		.increment('entries', 1)
-		.returning('entries')
-		.then((entries) => {
-			res.json(entries[0]);
-		})
-		.catch((err) => res.status(400).json('unable to get entries'));
+	image.handleImage(req, res, db);
+});
+
+app.post('/imageurl', (req, res) => {
+	image.handleApiCall(req, res);
 });
 
 app.listen(3001, () => {
